0e869b06dd58a3d234fb3078657426ae24f554d4
dimas-kenzo <kainbekas@gmail.com>
2022-07-19 12:06:59 +0700

    simple crud react-js

--- src/App.js
+++ src/App.js
@@ -1,84 +1,107 @@
-// import './App.css'
-import React, { useState, useEffect } from 'react'
-import RepoDetails from './RepoDetails'
-import axios from 'axios'
+import { useState } from "react";
+import "./App.css";
+import List from "./List";
+import { uid } from 'uid'
 
 function App() {
 
-  const [username, setUsername] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [repos, setRepos] = useState([])
-  const [details, setDetails] = useState({})
-  const [detailsLoading, setDetailsLoading] = useState(false)
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      name: 'Dimas Kenzo',
+      telp: '123456'
+    },
+    {
+      id: 2,
+      name: 'Kenan Quinno',
+      telp: '654321'
+    }
+  ])
 
-  useEffect(() => {
-    setRepos([])
-    setDetails({})
-  }, [username])
+  const [isUpdate, setIsUpdate] = useState({ id: null, status: false })
+  const [formData, setFormData] = useState({
+    name: '',
+    telp: ''
+  })
+
+
+  function handleChange(e) {
+    let data = { ...formData }
+    data[e.target.name] = e.target.value
+    setFormData(data)
+  }
 
   function handleSubmit(e) {
     e.preventDefault()
-    searchRepos()
+    let data = [...contacts]
+    if (formData.name === '') {
+      return false
+    }
+    if (formData.telp === '') {
+      return false
+    }
+    if (isUpdate.status) {
+      data.forEach((contact) => {
+        if (contact.id === isUpdate.id) {
+          contact.name = formData.name
+          contact.telp = formData.telp
+        }
+      })
+    } else {
+      data.push({ id: uid(), name: formData.name, telp: formData.telp })
+    }
+    setIsUpdate(false);
+    setContacts(data)
+    setFormData({ name: '', telp: '' })
   }
 
-  function searchRepos() {
-    setLoading(true)
-    axios({
-      method: 'get',
-      url: `https://api.github.com/users/${username}/repos`,
-    }).then(res => {
-      setLoading(false)
-      setRepos(res.data)
-    })
+  function handleEdit(id) {
+    // cari data di state
+    // isi data ke state form
+    let data = [...contacts];
+    console.log(setIsUpdate);
+    let foundData = data.find((contacts) => contacts.id === id);
+    setIsUpdate({ status: true, id: id });
+    setFormData({ name: foundData.name, telp: foundData.telp });
   }
 
-  function renderRepo(repo) {
-    return (
-      <div className="" key={repo.id} onClick={() => getDetails(repo.name)}>
-        <p style={{cursor:'pointer'}}>{repo.name}</p>
-      </div>
-    )
-  }
-
-  function getDetails(repoName) {
-    setDetailsLoading(true)
-    axios({
-      method: 'get',
-      url: `https://api.github.com/repos/${username}/${repoName}`,
-    }).then(res => {
-      setDetailsLoading(false)
-      setDetails(res.data)
-    })
+  function handleDelete(id) {
+    let data = [...contacts]
+    let filteredData = data.filter((contacts) => contacts.id !== id)
+    setContacts(filteredData)
   }
 
   return (
-    <div className="container p-3">
-      <div className="row">
-        <div className='col-md-7'>
-          <form className="row g-3">
-            <div className="col-auto">
-              <input className='form-control' value={username} placeholder='Github Username'
-                onChange={e => setUsername(e.target.value)} />
-            </div>
-            <div class="col-auto">
-              <button className='btn btn-primary' onClick={handleSubmit}>
-                {loading ? 'Searching ...' : 'Search'}
-              </button>     
-            </div>
-          </form>
-          <div className="p-3 link-primary">
-            {repos.map(renderRepo)}
-          </div>
-        </div>
-        <div className="col-md-4">
-          <RepoDetails details={details} loading={detailsLoading} />
-        </div>
-      </div>
-    </div>
+    <div className="App">
+      <h1 className="px-3 py-3">My Contact List</h1>
 
-  )
+      <form className="px-3 py-4" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="">Name</label>
+          <input type="text" className="form-control"
+            value={formData.name} name="name"
+            onChange={handleChange} />
+        </div>
+        <div className="form-group mt-3">
+          <label htmlFor="">No. Telp</label>
+          <input type="text" className="form-control"
+            value={formData.telp} name="telp"
+            onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary w-100 mt-3">
+            Save
+          </button>
+        </div>
+      </form>
+
+      <List
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        data={contacts}
+      />
+    </div>
+  );
 }
 
-export default App
-
-// yard add axios
+export default App;
